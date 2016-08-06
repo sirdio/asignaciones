6259c1e93fa8c62ff0dc9793077a58f75750079a
sirdio <sirdio24@hotmail.com>
2016-08-06 22:07:46 +0000

    reportes3

--- src/AppBundle/Controller/ReportesController.php
+++ src/AppBundle/Controller/ReportesController.php
@@ -32,8 +32,6 @@ class ReportesController extends Controller
             if ($request->isMethod('POST')) {
                 $em = $this->getDoctrine()->getManager();
                 $trabajo = $em->getRepository('AppBundle:Trabajo')->findBy(Array("niveltrab"=>$_POST['nivel']),Array("cantvoto"=>'ASC'));
-                echo count($trabajo);
-                die();
                 return $this->render('AppBundle:Reportes:resultadosvotos.html.twig', array("trabajo"=>$trabajo));
             }
             return $this->render('AppBundle:Reportes:seleccionarnivel.html.twig');
