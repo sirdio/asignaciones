1edc52c0193360c9c2996eeca8f772571030273a
sirdio <sirdio24@hotmail.com>
2016-08-12 14:46:33 +0000

    voto experiencia pedagogica6

--- src/AppBundle/Controller/AsistenciaController.php
+++ src/AppBundle/Controller/AsistenciaController.php
@@ -89,7 +89,7 @@ class AsistenciaController extends Controller
                     $encargado = $em->getRepository('AppBundle:Encargado')->findOneBy(Array("dni"=>$request->get('dni')));
                     $docente = $em->getRepository('AppBundle:Docente')->findOneBy(Array("dni"=>$request->get('dni')));
                     if($encargado){
-                        if($encargado->getCantvoto() == 0){
+                        if($encargado->getCantexpped() == 0){
                             $this->get('session')->getFlashBag()->add('mensaje','Usted ya superó la cantidad de voto disponible.');
                             return $this->render('AppBundle:Presentacion:verpresentacionexped.html.twig', array('expe'=>$presentacion));                            
                         }else{
